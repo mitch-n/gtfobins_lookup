bae6890f5c502cdb6a3b60405b983a92d0addbe3
mitch n <30005736+mitch-n@users.noreply.github.com>
2023-02-14 14:17:45 -0700

    Update gbl.sh

--- gbl.sh
+++ gbl.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 gtfobins_dir=/usr/share/gtfobins
+filter="."
+if [ $2 ]
+then 
+	functions=$(echo $2 | sed 's/,/|/')
+fi
+#echo "FUNCTION: $functions"
+
 
 function update_gtfobins () {
 	rm -rf $1/*
@@ -10,21 +17,24 @@ function update_gtfobins () {
 	rm -rf $1/temp	
 }
 
+
 if [ ! -d "$gtfobins_dir" ]
 then
 	mkdir $gtfobins_dir
 	update_gtfobins $gtfobins_dir
 fi
 
+
 cat $1 | while read line
 do
+	
 	tool_name=$(echo $line | rev | cut -d "/" -f1 | rev)
 	tool_path="$gtfobins_dir/$tool_name.md"
-	if [ -f "$tool_path" ]
+	if [ "$(grep $functions $tool_path 2>/dev/null)"  ]
 	then
 		echo $line
 		cat $tool_path | sed 's/^/    /'
 		echo ""
-	fi	
+	fi
 done
 
