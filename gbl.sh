861756f7f94045c528f7b9e16bb861f914c22c49
mitch n <30005736+mitch-n@users.noreply.github.com>
2023-02-14 14:25:06 -0700

    Update gbl.sh

--- gbl.sh
+++ gbl.sh
@@ -2,9 +2,10 @@
 
 gtfobins_dir=/usr/share/gtfobins
 filter="."
+
 if [ $2 ]
 then 
-	functions=$(echo $2 | sed 's/,/|/')
+	filter=$(echo $2 | sed 's/,/|/')
 fi
 #echo "FUNCTION: $functions"
 
@@ -30,7 +31,7 @@ do
 	
 	tool_name=$(echo $line | rev | cut -d "/" -f1 | rev)
 	tool_path="$gtfobins_dir/$tool_name.md"
-	if [ "$(grep $functions $tool_path 2>/dev/null)"  ]
+	if [ "$(grep $filter $tool_path 2>/dev/null)"  ]
 	then
 		echo $line
 		cat $tool_path | sed 's/^/    /'
