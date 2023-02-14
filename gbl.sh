027fe5b5e4c9eccda6c94aa6356560bbd4c9e3ae
mitch n <30005736+mitch-n@users.noreply.github.com>
2023-02-14 14:43:21 -0700

    Update gbl.sh

--- gbl.sh
+++ gbl.sh
@@ -3,13 +3,13 @@
 gtfobins_dir=/usr/share/gtfobins
 filter="."
 
+#Add filter if there is one supplied
 if [ $2 ]
 then 
 	filter=$(echo $2 | sed 's/,/|/')
 fi
-#echo "FUNCTION: $functions"
 
-
+#Define function to pull latest GTFO bins repo
 function update_gtfobins () {
 	rm -rf $1/*
 	mkdir $1/temp
@@ -18,17 +18,16 @@ function update_gtfobins () {
 	rm -rf $1/temp	
 }
 
-
+#Check if the gtfobins folder exists, if not, create it and updatedb
 if [ ! -d "$gtfobins_dir" ]
 then
 	mkdir $gtfobins_dir
 	update_gtfobins $gtfobins_dir
 fi
 
-
+#Search each tool and echo if there is a hit
 cat $1 | while read line
 do
-	
 	tool_name=$(echo $line | rev | cut -d "/" -f1 | rev)
 	tool_path="$gtfobins_dir/$tool_name.md"
 	if [ "$(grep $filter $tool_path 2>/dev/null)"  ]
